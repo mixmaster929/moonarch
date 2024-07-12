d215b8adc9b205496cf569d0e62a0602c0bc25af
mixmaster929 <smart.topdev929@gmail.com>
2024-07-12 06:28:37 +0200

    updated

--- scrap1.py
+++ scrap1.py
@@ -22,7 +22,7 @@ url = 'https://eth.moonarch.app/token/0xa8b229f7045192750a6aa25ccb088a196e713cd9
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get(url)
 
-token_check_messages = WebDriverWait(driver, 10).until(
+token_check_messages = WebDriverWait(driver, 100).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, 'token-check-message check-alert'))
 )
 
